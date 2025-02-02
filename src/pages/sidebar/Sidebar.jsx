da44f32e4c32d0b54e6996af19296edd300e5f4f
Dongwon KIm <dongwon000103@gmail.com>
2025-02-02 21:42:03 +0900

    design: 로그인 디자인 수정

--- src/pages/sidebar/Sidebar.jsx
+++ src/pages/sidebar/Sidebar.jsx
@@ -1,27 +1,26 @@
-import useAuthStore from "../../store/authStore";
-import SidebarLogin from "./SidebarLogin";
-import SidebarLogout from "./SidebarLogout";
-import Logo from '../../assets/Logo.png';
+import useAuthStore from '../../store/authStore';
+import SidebarLogin from './SidebarLogin';
+import SidebarLogout from './SidebarLogout';
+import { ReactComponent as Logo } from '../../assets/Logo.svg';
+import { ReactComponent as KoStarter } from '../../assets/KoStarter.svg';
 
 const Sidebar = () => {
-  const { isLogin } = useAuthStore();
+    const { isLogin } = useAuthStore();
 
-  return (
-    <div
-      className="w-64 h-screen bg-[#FFFFFF] text-white flex flex-col items-center fixed top-0 left-0"
-      style={{ paddingTop: '15px' }}
-    >
-      {/* 로고와 텍스트 */}
-      <div className="flex items-center mt-[15px]">
-        <img src={Logo} alt="Kinda Logo" className="w-10 h-10 rounded-lg mr-2" />
-        <h1 className="text-2xl font-bold text-[#3B3B3B]" style={{ fontSize: '37px' }}>
-          KoStarter
-        </h1>
-      </div>
+    return (
+        <div
+            className="w-64 h-screen bg-[#FFFFFF] text-white flex flex-col items-center fixed top-0 left-0"
+            style={{ paddingTop: '15px' }}
+        >
+            {/* 로고와 텍스트 */}
+            <a href="/" className="flex my-4 items-center justify-center items-center h-8 gap-2">
+                <Logo className="w-6" />
+                <KoStarter className="w-30" />
+            </a>
 
-      {isLogin ? <SidebarLogin /> : <SidebarLogout />}
-    </div>
-  );
+            {isLogin ? <SidebarLogin /> : <SidebarLogout />}
+        </div>
+    );
 };
 
 export default Sidebar;
